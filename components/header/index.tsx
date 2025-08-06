f0c18c140ee1dbab6523e9830279105a5e5de945
MohammadAmin Sangtarash <msangtarash@basalam.in>
2025-08-06 11:01:55 +0330

    responsive bugs fixed

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,15 +1,41 @@
+'use client'
+import MenuIcon from "@/icons/menu";
 import Link from "next/link";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 
 function Header() {
+    const [open, setOpen] = useState(false);
+
+    const handleToggle = () => {
+        setOpen(prev => !prev)
+    }
+
     return (
-        <div className="flex items-center h-14 bg-linear-to-bl from-[var(--secondary)] to-[var(--primary)] text-white p-2 rounded-lg mb-4">
-            <Link className="mx-3 p-3" href="/" >خانه</Link>
-            <Link className="mx-3 p-3" href="/products" >محصولات</Link>
-            <Link className="mx-3 p-3" href="/vendors" >غرفه ها</Link>
-            <Link className="mx-3 p-3" href="/tags" >تگ ها</Link>
-            <Link className="mx-3 p-3" href="/logistic" >لجستیک</Link>
-        </div>
+        <>
+            <div className="hidden md:flex items-center h-14 bg-linear-to-bl from-[var(--secondary)] to-[var(--primary)] text-white p-2 rounded-lg mb-4">
+                <Link className="mx-3 p-3" href="/">خانه</Link>
+                <Link className="mx-3 p-3" href="/products">محصولات</Link>
+                <Link className="mx-3 p-3" href="/vendors">غرفه ها</Link>
+                <Link className="mx-3 p-3" href="/tags">تگ ها</Link>
+                <Link className="mx-3 p-3" href="/logistic">لجستیک</Link>
+            </div>
+            <div className={twMerge((open ? "right-0" : "right-[-100vw]"), "w-[100vw] h-[100vh] fixed top-0 bg-linear-to-bl from-[var(--secondary)] to-[var(--primary)] flex md:hidden flex-col items-center z-1020 text-white")}>
+                <div className="w-100 flex justify-end px-13 py-3 relative mb-10">
+                    <div className={twMerge((open ? "left-0" : "left-[-60px]"), "absolute rounded-sm bg-linear-to-bl from-[var(--secondary)] to-[var(--primary)]")} onClick={handleToggle}>
+                        <MenuIcon className="w-10" />
+                    </div>
+                </div>
+                <div onClick={handleToggle} className="flex flex-col items-center">
+                    <Link className="mx-3 p-3" href="/">خانه</Link>
+                    <Link className="mx-3 p-3" href="/products">محصولات</Link>
+                    <Link className="mx-3 p-3" href="/vendors">غرفه ها</Link>
+                    <Link className="mx-3 p-3" href="/tags">تگ ها</Link>
+                    <Link className="mx-3 p-3" href="/logistic" >لجستیک</Link>
+                </div>
+            </div>
+        </>
     );
 }
 
---
+++ icons/menu.tsx
@@ -0,0 +1,11 @@
+
+function MenuIcon({ color = "#333333", ...props }) {
+    return (
+        <svg viewBox="0 0 30 30" fill="none" {...props}>
+            <path fillRule="evenodd" clipRule="evenodd" d="M3.75 10V7.5H26.25V10H3.75ZM3.75 16.25H26.25V13.75H3.75V16.25ZM3.75 22.5H26.25V20H3.75V22.5Z" fill="currentColor" />
+        </svg>
+    );
+}
+
+export default MenuIcon;
+
