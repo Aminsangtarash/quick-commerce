61b5d9eb416a0823278a7175024fef10490ab8ee
MohammadAmin Sangtarash <msangtarash@basalam.in>
2025-08-16 13:01:59 +0330

    logo added

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,21 @@
 import { Suspense } from "react";
 import VendorsTable from "../components/vendors-table";
 import Loading from "@/components/loading";
+import logo from "/public/logo.svg"
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex justify-center py-10 sm:py-20 sm:px-20">
       <main className="flex flex-col max-w-full items-center sm:items-start">
-        <h1
-          className="text-[70px] font-black text-center w-full mb-10"
-        >
-          Quick Commerce
-        </h1>
+        <div className="flex items-center justify-center mb-10 w-full">
+          <h1
+            className="text-[70px] font-black"
+          >
+            Quick Commerce
+          </h1>
+          <Image height={80} width={80} src={logo} alt="لوگو" className="mr-3"/>
+        </div>
         <div className="flex gap-4 items-center max-w-full flex-col sm:flex-row">
           <Suspense fallback={<Loading />}>
             <VendorsTable />
